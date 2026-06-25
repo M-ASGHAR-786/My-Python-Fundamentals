3efb1809be3933397ed79e903288f9598645df34
M-ASGHAR-786 <masgharali1933@gmail.com>
2026-06-25 21:18:45 +0500

    Demonstrated Comments with basic Code

---
+++ Comments/comments.py
@@ -0,0 +1,39 @@
+print("This will be printed")
+
+# print("This will not be printed")                     |----> This was single line Comment in python
+
+
+# ------------------------------------------------------------------------------------------------------------------------#
+
+
+print('''
+      I will write the content and
+      this will be printed in 
+      terminal and no errors would occur''')
+
+
+# -------------------------------------------------------------------------------------------------------------------------#
+
+
+# print('''                                             |
+#         I will write the content and                  |       This is multi-line comment which is rarely used by any
+#         this will not be printed in                   |---->   dev nowadays because of advanced and more reliable
+#         terminal, cause python will ignore it''')     |       features of IDEs, it`s much common methods for multi-`
+#                                                       |                           line comment.
+#                                                       |
+
+
+# -------------------------------------------------------------------------------------------------------------------------#
+
+
+"""                                                     |
+                                                        |      This is multi-line comment which is not commonly used by
+    print('''                                           |         dev nowadays because of advanced and more reliable 
+        I will write the content and                    |----> features 0f IDEs. It was not meant for multi-line comments
+        this will not be printed in                     |                          to be honest!            
+        terminal, cause python will ignore it''')       |     
+                                                        |
+"""
+
+
+# -------------------------------------------------------------------------------------------------------------------------#
