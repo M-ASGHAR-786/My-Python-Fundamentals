8923becf082a3b8e9a8ad7f4057af563b7a2b15e
M-ASGHAR-786 <masgharali1933@gmail.com>
2026-06-26 16:20:05 +0500

    Added var_rules.py with variable naming rules and examples.

---
+++ 1. initial_programs/genesis.py
@@ -0,0 +1,2 @@
+print("Hello Me!")
+print("I hope you are at least an amazing Dev😅 by now")
---
+++ 3. Variables/var_rules.py
@@ -0,0 +1,19 @@
+# A variable is like a container for storing data values.
+# Variable Naming Rules:
+# - Variable names must start with a letter or an underscore.
+# - Variable names cannot start with a number.
+# - Variable names can only contain alpha-numeric characters and underscores (a-z, 0-9, and _).
+# - Variable names are case-sensitive (age, Age, and AGE are three different variables).
+# - Variable names cannot contain whitespace(empty space).
+# - Standard Python function names or keywords cannot be used as variable names(such as print, if, else, and int etc. ).
+
+
+a = 2                          # Valid
+name = "Asghar"                # Valid
+# $car = "Not Available"       # Invalid because, var only begins with underscore(_) or Alphabet
+_age = 21  # Valid
+# print = "file"               # Invalid, built-in or pre-defined functions can't be used
+fav_content = "Anime"          # Valid
+fav_season = "Winter"
+# $semester = "1st"            # Invalid, because var can't begin with special character
+# fav thing = "watching anime" # Invalid, because can't contain white space in between
